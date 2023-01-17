785fa77ac6a1aed053e1fe6ccc5504addf23c58c
Joaquin Montes <joaquin@basement.studio>
2023-01-17 13:38:11 -0300

    refactor: migrate to mdx files

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'docs-test' {
-  export function getMarkdownSlugs(): Promise<string[]>;
-  export function getAbsolutePath(name: string): Promise<string>;
-  export function getFiles(folder: string, depth: number): Promise<string[]>;
-}
